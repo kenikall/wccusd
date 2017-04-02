aaf6b00ade26fbb85121e3e57f3eb3e20ba7e6c6
Mannah <kenikall@gmail.com>
2017-04-02 15:16:19 -0700

    setup bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :views
   devise_for :users
+
+  resources :users, only: [:index, :show, :edit, :update]
+
   devise_scope :user do
     root to: "devise/sessions#new"
   end
