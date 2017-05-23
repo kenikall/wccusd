a0d690075d365124e871422c92986afe3649600d
Mannah <kenikall@gmail.com>
2017-05-23 06:26:31 -0700

    creates and styles calendar

---
+++ app/assets/javascripts/calendar.js
@@ -0,0 +1,10 @@
+$(document).ready(function( event ){
+  console.log("calendar");
+  $(".day").click(function (){
+    var clickedDay = this;
+    console.log(clickedDay);
+    if(clickedDay.hasClass(future)){
+    }
+  })
+  $()
+})
---
+++ app/assets/javascripts/pick_date.js
@@ -0,0 +1,7 @@
+$(document).ready(function( event ){
+  console.log("pick_date");
+  $(".day").click(function (){
+    console.log(this);
+
+  })
+})
