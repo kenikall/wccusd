5871b3af375c2696a7a6101530e0b3ca8f15b1fb
Mannah <kenikall@gmail.com>
2017-07-21 07:02:13 -0700

    WIP

--- app/assets/javascripts/student_field.js
+++ app/assets/javascripts/student_field.js
@@ -1,8 +1,9 @@
 document.addEventListener("turbolinks:load", function( event ){
-  $("#add-student-button").on("click", ".unselected-student", function ( event ){
-    if $("add-student-field").hasClass("hidden"){
-      $("add-student-field").slideToggle();
-    }
-    $("submit-student-field").slideToggle();
+  $("#add-student-button").click( function ( event ){
+    console.log("click");
+    // if $("add-student-field").hasClass("hidden"){
+      // $("add-student-field").slideToggle();
+    // }
+    // $("submit-student-field").slideToggle();
   })
 })
